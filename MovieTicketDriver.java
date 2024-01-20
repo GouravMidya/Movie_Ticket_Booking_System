65117ba341d2802f3db0eae665b666d51f25d804
GouravMidya <gouravgmidya@gmail.com>
2024-01-20 15:26:10 +0530

    First Commit , Basic Skeletion

---
+++ MovieTicketDriver.java
@@ -0,0 +1,35 @@
+class User{
+    String name,username,password,address,phone;
+
+    void buyTicket(){
+
+    }
+    void showTicket(){
+    
+    }
+
+    void cancelTicket(){
+
+    }
+
+    void logout(){
+
+    }
+}
+
+class Admin extends User{
+    void addMovies(){
+
+    }
+
+    void selectMovies(){
+        
+    }
+}
+
+
+public class MovieTicketDriver {
+    public static void main(String[] args) {
+        
+    }
+}
