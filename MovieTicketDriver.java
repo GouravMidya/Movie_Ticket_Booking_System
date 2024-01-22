61ac27da00864ba802582d6080424dcdab3b4c47
GouravMidya <gouravgmidya@gmail.com>
2024-01-22 23:38:31 +0530

    enriched shwotime and theater class with insert and show methods

--- MovieTicketDriver.java
+++ MovieTicketDriver.java
@@ -1,9 +1,28 @@
+import java.time.*;
+import java.sql.*;
+
 public class MovieTicketDriver {
     public static void main(String[] args) {
         //Movie m = new Movie("Race 3","Action",4.9,120,"Its a good movie of racing");
         //m.insertMovie();
 
-        Movie m = new Movie();
-        m.showMovies();
+        //Movie m = new Movie();
+        //m.showMovies();
+
+        //Theater t = new Theater();
+        //t.insertTheater("IMAX",40);
+        //t.showTheaters();
+
+        Showtime s = new Showtime();
+        // Create a LocalDateTime object for today at 4 PM
+        LocalDateTime localDateTime = LocalDateTime.now().withHour(16).withMinute(0).withSecond(0);
+
+        // Convert the LocalDateTime to a Timestamp
+        Timestamp showtime = Timestamp.from(localDateTime.atZone(ZoneId.systemDefault()).toInstant());
+
+
+        // Call the insertShowtime method
+        //s.insertShowtime(1, 2, showtime);
+        s.showShowtimes();
     }
 }
--- Showtime.java
+++ Showtime.java
@@ -1,3 +1,29 @@
+import java.sql.Timestamp;
+import java.util.*;
+
 public class Showtime {
+
+    DatabaseOperation db = new DatabaseOperation();
+    public void insertShowtime(int MovieID,int TheaterID,Timestamp showtime){
+        String sql = "INSERT INTO Showtimes(MovieID,TheaterID,Showtime) VALUES(?,?,?)";
+        Object[] values = {MovieID,TheaterID,showtime};
+        int rowsAffected = db.executeUpdate(sql, values);
+        if(rowsAffected>0)
+            System.out.println("Showtime added successfully");
+        else
+            System.out.println("Something went wrong.Showtime not inserted.");
     
+    }
+
+    public void showShowtimes(){
+        String sql = "SELECT * from Showtimes";
+        List<Map<String,Object>> showtimes = db.getRecords(sql);
+        for (Map<String, Object> showtime : showtimes) {
+            System.out.println("Showtime ID: " + showtime.get("ShowtimeID"));
+            System.out.println("Movie ID: " + showtime.get("MovieID"));
+            System.out.println("Theater ID: " + showtime.get("TheaterID"));
+            System.out.println("Showtime: " + showtime.get("Showtime"));
+            System.out.println("-----------------------------"); 
+        }
+    }
 }
